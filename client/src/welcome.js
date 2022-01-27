1c8f9d49d67a94164f7d25ae74db57a3cadaf844
Stefan Pfeuffer <mail@stefanpfeuffer.com>
2022-01-27 16:01:20 +0100

    can login

--- client/src/welcome.js
+++ client/src/welcome.js
@@ -1,15 +1,19 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import Registration from "./auth-components/registration";
+import Login from "./auth-components/login";
 
 export default function Welcome() {
     return (
-        <div className="logged-out-wrapper">
-            <h1>Welcome To Our Site</h1>
-            <BrowserRouter>
+        <BrowserRouter>
+            <div className="logged-out-wrapper">
+                <h1>Welcome To Our Site</h1>
                 <Route exact path="/">
                     <Registration />
                 </Route>
-            </BrowserRouter>
-        </div>
+                <Route path="/login">
+                    <Login />
+                </Route>
+            </div>
+        </BrowserRouter>
     );
 }
